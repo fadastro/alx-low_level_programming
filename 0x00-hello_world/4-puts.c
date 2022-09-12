cea46d4d41668263e479b9fb08e9623a16944c4d
fadastro <fadastro@gmail.com>
2022-09-12 09:33:50 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,6 @@
  *main - the main running block
  *return: 0
  */
-
-/**
- * main - to print string
- * @void: nothing to return
- * 
- */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

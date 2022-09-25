c7872cb8eb602709dad14c6ba92675673d8e73a3
Fadastro <fadastro@gmail.com>
2022-09-25 19:04:22 +0200

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+/*
+* File: 102-print_comb5.c
+* Auth: Farai Simba
+*/
+#include <stdio.h>
+
+/**
+* main - Entry point
+* description - This program print trhee combibnation digits
+* Return: Always success(0)
+*/
+int main(void)
+{
+int num1, num2;
+
+for (num1 = 0; num1 <= 98; num++)
+{
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n')
+return (0);
+}

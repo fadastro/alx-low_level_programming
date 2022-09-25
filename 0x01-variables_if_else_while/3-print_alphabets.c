37e866d9158fad2b7975ce607213e26c9a719c08
Fadastro <fadastro@gmail.com>
2022-09-25 16:53:43 +0200

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+
+/*
+* File: 3-print_alphabets.c
+* Auth: Farai Simba
+*/
+#include <stdlib.h>
+
+/**
+* main - Prints the alphabets in lowercase, and the in uppercase.
+* Return: Always 0
+*/
+int main(void)
+{
+
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+
+putchar('\n');
+return (0);
+}

f3825c7acd3b7fc0ca15c38072927a4ec4f767de
Fadastro <fadastro@gmail.com>
2022-09-25 11:48:36 +0200

    is n negarive or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
+/*
  * This program check if a number is a negative, positve or zero
-*/
+ */
 
-/**
+/*
  * @main: returns 0
-*/
+ */
 int main(void)
 {
     int n;
@@ -15,14 +15,14 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2; 
 
-    if(n == 0)
-        printf("%d is zero\n", n); /** prints out n is zero */
+    if (n == 0)
+        printf("%d is zero\n", n); /* prints out n is zero */
 
-    if(n > 0)
-        printf("%d is positive\n", n); /** prints out n is positive */
+    if (n > 0)
+        printf("%d is positive\n", n); /* prints out n is positive */
 
-    if(n < 0)
-        printf("%d is negative\n", n); /** prints out n is negative */
+    if (n < 0)
+        printf("%d is negative\n", n); /* prints out n is negative */
 
     return (0);
 }

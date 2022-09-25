80bdefa8be766fb2c9e1777d4a42e2db2db51c4a
Fadastro <fadastro@gmail.com>
2022-09-25 16:51:37 +0200

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,32 @@
+/*
+* File: 3-print_alphabets.c
+* Auth: Farai Simba
+*/
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * 
- * main - Entry point
- * Description: This program print the last number of integer
- * 
- * Return: it returns 0
- * 
+* main - Entry point
+* description - this program print the last number of integer
+* Return: it returns 0
 */
 int main(void)
 {
-    int n;
-    int lastdigit;
+int n;
+int lastdigit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    lastdigit = n % 10;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastdigit = n % 10;
+if (lastdigit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 
-    if (lastdigit > 5)
-        printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+if (lastdigit == 0)
+printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 
-    if (lastdigit == 0)
-        printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+if (lastdigit < 6 && lastdigit != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 
-    if (lastdigit < 6 && lastdigit != 0)
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
-    
-    return (0);
+return (0);
 }
-   

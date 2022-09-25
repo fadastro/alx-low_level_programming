28af1ed41a1c00d8e7a30c3d24fff868a8272e19
Fadastro <fadastro@gmail.com>
2022-09-25 17:10:41 +0200

    All alphabets except e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Farai Simba
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* description - this program print all the alphabets except q and e
+* Return: Always success
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter < 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}

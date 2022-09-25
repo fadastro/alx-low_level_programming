0683452dfe534baf847e69c3d52c1faa2fe63774
Fadastro <fadastro@gmail.com>
2022-09-25 14:18:29 +0200

    Alphabet Program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * manin - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    cahr c = 'a';
+
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+
+    return (0);
+}

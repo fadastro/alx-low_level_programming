1af8a555fc3bf4b593fb444052d4cd0c0a60019d
Fadastro <fadastro@gmail.com>
2022-09-25 17:24:55 +0200

    print number from 0 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,15 @@
-/***/
+/*
+* File: 5-print_numbers.c
+* Auth: Farai Simba
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-/***/
+/**
+* main - Entry Point
+* description - the program print numbers from 0 to 9
+* Return: Always success(0)
+*/
 int main(void)
 {
 int n;
